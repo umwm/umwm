d33c6a110bbd4bfd60aa74f518db7b617b544f99
milancurcic <caomaco@gmail.com>
2020-06-19 14:18:26 -0400

    bug fix and clean up in wrf2umwmgrid

--- tools/src/grid_utils.f90
+++ tools/src/grid_utils.f90
@@ -1,170 +1,134 @@
-! University of Miami Wave Model (UMWM)
-! grid_utils
-! Copyright (C) 2013  Milan Curcic
-!
-! This program is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
-!
-! This program is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with this program.  If not, see <http://www.gnu.org/licenses/>.
-!
-!======================================================================!
-MODULE grid_utils
-!=======================================================================
+module grid_utils
 
-IMPLICIT NONE
+implicit none
 
-PRIVATE
+private
+public :: refineField, coarsenField
 
-PUBLIC :: refineField
-PUBLIC :: coarsenField
-
-!=======================================================================
-CONTAINS
+contains
 
 
-SUBROUTINE refineField(field_src,field_dst,ratio)
-!=======================================================================
+subroutine refineField(field_src,field_dst,ratio)
+  real, intent(in) :: field_src(:,:)
+  real, intent(out) :: field_dst(:,:)
+  integer, intent(in) :: ratio
 
-! Dummy arguments:
-REAL,DIMENSION(:,:),INTENT(IN)  :: field_src
-REAL,DIMENSION(:,:),INTENT(OUT) :: field_dst
-INTEGER,            INTENT(IN)  :: ratio
+  integer :: i,j
+  integer :: i2,j2
+  integer :: isub,jsub
+  integer :: im,jm
+  integer :: im_dst,jm_dst
+  integer :: im_src,jm_src
 
-INTEGER :: i,j
-INTEGER :: i2,j2
-INTEGER :: isub,jsub
-INTEGER :: im,jm
-INTEGER :: im_dst,jm_dst
-INTEGER :: im_src,jm_src
+  real :: a,b
+  real :: w1,w2,w3,w4
 
-REAL :: a,b
-REAL :: w1,w2,w3,w4
+  if (ratio == 1) then
+    field_dst = field_src
+    return
+  end if
 
-!=======================================================================
+  ! Get size of source field:
+  im_src = size(field_src, dim=1)
+  jm_src = size(field_src, dim=2)
 
-IF(ratio == 1)THEN
-  field_dst = field_src
-  RETURN
-ENDIF
+  ! Compute the size of target field:
+  im = (im_src-1) *ratio + 1
+  jm = (jm_src-1) *ratio + 1
 
-! Get size of source field:
-im_src = SIZE(field_src,DIM=1)
-jm_src = SIZE(field_src,DIM=2)
+  ! Get size of the target field dummy argument:
+  im_dst = size(field_dst, dim=1)
+  jm_dst = size(field_dst, dim=2)
 
-! Compute the size of target field:
-im = (im_src-1)*ratio+1
-jm = (jm_src-1)*ratio+1
+  if (im_dst > im .or. jm_dst > jm) then
+    write(*,*) 'RefineField: ERROR: &
+                Source field smaller than required by the target field size:'
+    write(*,*) 'Source field:                im_src,jm_src = ',im_src,jm_src
+    write(*,*) 'Interpolates to:                    im,jm  = ',im,jm
+    write(*,*) 'Target field dummy argument: im_dst,jm_dst = ',im_dst,jm_dst
+    return
+  end if
 
-! Get size of the target field dummy argument:
-im_dst = SIZE(field_dst,DIM=1)
-jm_dst = SIZE(field_dst,DIM=2)
+  field_dst = 0
 
-IF(im_dst >im .OR. jm_dst > jm)THEN
-  WRITE(*,*)'RefineField: ERROR: &
-             Source field smaller than required by the target field size:'
-  WRITE(*,*)'Source field:                im_src,jm_src = ',im_src,jm_src
-  WRITE(*,*)'Interpolates to:                    im,jm  = ',im,jm
-  WRITE(*,*)'Target field dummy argument: im_dst,jm_dst = ',im_dst,jm_dst
-  RETURN
-ENDIF
+  ! Outer loop over the coarse grid:
+  do j=1,jm_src-1
+    do i=1,im_src-1
+      i2 = (i-1)*ratio+1
+      j2 = (j-1)*ratio+1
+      do jsub=0,ratio-1
+        do isub=0,ratio-1
+          a = 1.-isub/real(ratio)
+          b = 1.-jsub/real(ratio)
+          w1 = a*b
+          w2 = b*(1.-a)
+          w3 = a*(1.-b)
+          w4 = (1.-a)*(1.-b)
+          field_dst(i2+isub,j2+jsub) = w1*field_src(i  ,j  )&
+                                      +w2*field_src(i+1,j  )&
+                                      +w3*field_src(i  ,j+1)&
+                                      +w4*field_src(i+1,j+1)
+        end do
+      end do
+    end do
+  end do
 
-field_dst = 0
-
-! Outer loop over the coarse grid:
-DO j=1,jm_src-1
-  DO i=1,im_src-1
+  ! Close last row:
+  do i=1,im_src-1
     i2 = (i-1)*ratio+1
+    do isub=0,ratio-1
+      w1 = 1.-isub/real(ratio)
+      w2 = 1.-w1
+      field_dst(i2+isub,jm) = w1*field_src(i,jm_src)&
+                             +w2*field_src(i+1,jm_src)
+    end do
+  end do
+
+  ! Close last column:
+  do j=1,jm_src-1
     j2 = (j-1)*ratio+1
-    DO jsub=0,ratio-1
-      DO isub=0,ratio-1
-        a = 1.-isub/FLOAT(ratio)
-        b = 1.-jsub/FLOAT(ratio)
-        w1 = a*b
-        w2 = b*(1.-a)
-        w3 = a*(1.-b)
-        w4 = (1.-a)*(1.-b)
-        field_dst(i2+isub,j2+jsub) = w1*field_src(i  ,j  )&
-                                    +w2*field_src(i+1,j  )&
-                                    +w3*field_src(i  ,j+1)&
-                                    +w4*field_src(i+1,j+1)
-      ENDDO
-    ENDDO
-  ENDDO
-ENDDO
+    do jsub=0,ratio-1
+      w1 = 1.-jsub/real(ratio)
+      w2 = 1.-w1
+      field_dst(im,j2+jsub) = w1*field_src(im_src,j)&
+                             +w2*field_src(im_src,j+1)
+    end do
+  end do
 
-! Close last row:
-DO i=1,im_src-1
-  i2 = (i-1)*ratio+1
-  DO isub=0,ratio-1
-    w1 = 1.-isub/FLOAT(ratio)
-    w2 = 1.-w1
-    field_dst(i2+isub,jm) = w1*field_src(i,jm_src)&
-                           +w2*field_src(i+1,jm_src)
-  ENDDO
-ENDDO
+  ! Close top-right corner:
+  field_dst(im,jm) = field_src(im_src, jm_src)
 
-! Close last column:
-DO j=1,jm_src-1
-  j2 = (j-1)*ratio+1
-  DO jsub=0,ratio-1
-    w1 = 1.-jsub/FLOAT(ratio)
-    w2 = 1.-w1
-    field_dst(im,j2+jsub) = w1*field_src(im_src,j)&
-                           +w2*field_src(im_src,j+1)
-  ENDDO
-ENDDO
-
-! Close top-right corner:
-field_dst(im,jm) = field_src(im_src,jm_src)
-
-ENDSUBROUTINE refineField
-!=======================================================================
+end subroutine refineField
 
 
+subroutine coarsenField(field_src, field_dst, ratio)
+  real, intent(in) :: field_src(:,:)
+  real, intent(out) :: field_dst(:,:)
+  integer, intent(in) :: ratio
 
-SUBROUTINE coarsenField(field_src,field_dst,ratio)
-!=======================================================================
+  integer :: i,j
+  integer :: isub,jsub
+  integer :: im,jm
 
-! Dummy arguments:
-REAL,DIMENSION(:,:),INTENT(IN)  :: field_src
-REAL,DIMENSION(:,:),INTENT(OUT) :: field_dst
-INTEGER,            INTENT(IN)  :: ratio
+  integer :: stride
 
-INTEGER :: i,j
-INTEGER :: isub,jsub
-INTEGER :: im,jm
+  real :: invNumPoints
 
-INTEGER :: stride
+  invNumPoints = (1 / dble(ratio))**2
+  stride = ratio - 1
 
-REAL :: invNumPoints
+  im = size(field_dst, dim=1)
+  jm = size(field_dst, dim=2)
 
-!=======================================================================
+  ! Loop over the coarse grid and average values from the fine grid:
+  do j = 1, jm
+    jsub = (j - 1) * ratio + 1
+    do i = 1, im
+      isub = (i - 1) * ratio + 1
+      field_dst(i,j) = sum(field_src(isub:isub+stride,jsub:jsub+stride)) * invNumPoints
+    end do
+  end do
 
-invNumPoints = (1./DBLE(ratio))**2.
-stride = ratio-1
+end subroutine coarsenField
 
-im = SIZE(field_dst,DIM=1)
-jm = SIZE(field_dst,DIM=2)
-
-! Loop over the coarse grid and average values from the fine grid:
-DO j=1,jm
-  jsub = (j-1)*ratio+1
-  DO i=1,im
-    isub = (i-1)*ratio+1
-    field_dst(i,j) = SUM(field_src(isub:isub+stride, &
-                                   jsub:jsub+stride))&
-                    *invNumPoints
-  ENDDO
-ENDDO
-
-ENDSUBROUTINE coarsenField
-!=======================================================================
-ENDMODULE grid_utils
+end module grid_utils
--- tools/src/wrf2umwmgrid.f90
+++ tools/src/wrf2umwmgrid.f90
@@ -1,217 +1,156 @@
-!
-! University of Miami Wave Model (UMWM)
-! wrf2umwmgrid
-! Copyright (C) 2012  Milan Curcic
-!
-! This program is free software: you can redistribute it and/or modify
-! it under the terms of the GNU General Public License as published by
-! the Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
-!
-! This program is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License
-! along with this program.  If not, see <http://www.gnu.org/licenses/>.
-!
-!======================================================================!
-PROGRAM wrf2umwmgrid
-!=======================================================================
-!
-! DESCRIPTION: Utility program that reads lon, lat and mask fields
-! from a WRF file, and writes them into a umwm.grid file which can be
-! read from umwm_topogen for topography generation.
-!              
-! REVISION HISTORY:
-!              
-! Oct 2013, 1.1.0, Added the option to refine or coarsen grid by an
-!                  integer factor
-! Jun 2012, 1.0.1, First public release
-! 
-!=======================================================================
-USE netcdf
-USE umwm_utils,ONLY:nc_check
-USE grid_utils,ONLY:refineField,coarsenField
-!=======================================================================
-IMPLICIT NONE
+program wrf2umwmgrid
+  ! Reads longitude, latitude, and landmask from a WRF file 
+  ! and writes them into a umwm.grid file.
+  use netcdf
+  use umwm_utils, only: nc_check
+  use grid_utils, only: refineField, coarsenField
+  implicit none
 
-INTEGER :: i,j
-INTEGER :: idm_src,jdm_src
-INTEGER :: idm_dst,jdm_dst
+  integer :: i, j
+  integer :: idm_src, jdm_src
+  integer :: idm_dst, jdm_dst
 
-INTEGER :: stat
-INTEGER :: ncid,xdimid,ydimid,lonid,latid,maskid
+  integer :: stat, ncid, xdimid, ydimid, lonid, latid, maskid
 
-CHARACTER(LEN=9999) :: wrfInputFile
-CHARACTER(LEN=2)    :: refinementFactorStr
+  character(9999) :: wrfInputFile
+  character(2) :: refinementFactorStr
 
-CHARACTER(LEN=NF90_MAX_NAME) :: dimname
+  character(NF90_MAX_NAME) :: dimname
 
-REAL,DIMENSION(:,:),ALLOCATABLE :: lon_src,lat_src
-REAL,DIMENSION(:,:),ALLOCATABLE :: lon_dst,lat_dst
+  real, allocatable :: lon_src(:,:), lat_src(:,:)
+  real, allocatable :: lon_dst(:,:), lat_dst(:,:)
 
-INTEGER,DIMENSION(:,:),ALLOCATABLE :: mask_src
-REAL,   DIMENSION(:,:),ALLOCATABLE :: mask_dst
+  integer, allocatable :: mask_src(:,:)
+  real, allocatable :: mask_dst(:,:)
 
-INTEGER :: refinementFactor = 1
+  integer :: refinementFactor = 1
 
-!=======================================================================
+  call get_command_argument(1, value=wrfInputFile, status=stat)
 
-! 1) Get command line argument:
-CALL GET_COMMAND_ARGUMENT(1,VALUE=wrfInputFile,STATUS=stat)
+  ! Error checking:
+  if (stat /=0 ) then
+    write(*,*) 'wrf2umwmgrid: Error: Cannot retrieve argument 1'
+    stop
+  else
+    write(*,*) 'wrf2umwmgrid: Processing file ' // trim(wrfInputFile)
+  end if
 
-! Error checking:
-IF(stat /=0 )THEN
-  WRITE(*,*)'wrf2umwmgrid: Error: Cannot retrieve argument 1'
-  STOP
-ELSE
-  WRITE(*,*)'wrf2umwmgrid: Processing file '//TRIM(wrfInputFile)
-ENDIF
+  call get_command_argument(2, value=refinementFactorStr, status=stat)
+  if (stat == 0) then
+    read(refinementFactorStr,*) refinementFactor
+  end if
 
-CALL GET_COMMAND_ARGUMENT(2,VALUE=refinementFactorStr,STATUS=stat)
-IF(stat == 0)THEN
-  READ(UNIT=refinementFactorStr,FMT=*)refinementFactor
-ENDIF
+  if (refinementFactor == 0)then
+    write(*,*) 'wrf2umwmgrid: Error: refinementFactor must be a non-zero integer'
+    stop
+  end if
 
-IF(refinementFactor == 0)THEN
-  WRITE(*,*)'wrf2umwmgrid: Error: refinementFactor must be a non-zero integer'
-  STOP
-ENDIF
+  if (refinementFactor >= 1) then
+    write(*,'(a,I2)') 'wrf2umwmgrid: Refinement factor: ', refinementFactor
+  else if(refinementFactor < 0)THEN
+    write(*,'(a,I2)') 'wrf2umwmgrid: Refinement factor: 1 /', - refinementFactor
+  end if
 
-IF(refinementFactor >= 1)THEN
-  WRITE(*,FMT='(A,I2)')'wrf2umwmgrid: Refinement factor: ',refinementFactor
-ELSEIF(refinementFactor < 0)THEN
-  WRITE(*,FMT='(A,I2)')'wrf2umwmgrid: Refinement factor: 1 /',-refinementFactor
-ENDIF
+  write(*,*) 'wrf2umwmgrid: Reading input (WRF) grid.'
 
-!=======================================================================
+  call nc_check(nf90_open(trim(wrfInputFile), NF90_NOWRITE, ncid))
+  call nc_check(nf90_inq_dimid(ncid, 'west_east', xdimid))
+  call nc_check(nf90_inq_dimid(ncid, 'south_north', ydimid))
+  call nc_check(nf90_inquire_dimension(ncid, xdimid, dimname, idm_src))
+  call nc_check(nf90_inquire_dimension(ncid, ydimid, dimname, jdm_src))
 
-! 2) Read input grid:
-WRITE(*,*)'wrf2umwmgrid: Reading input (WRF) grid.'
+  write(*,*) 'wrf2umwmgrid: WRF grid idm/jdm:', idm_src, jdm_src
 
-CALL nc_check(nf90_open(TRIM(wrfInputFile),nf90_nowrite,ncid))
-CALL nc_check(nf90_inq_dimid(ncid,'west_east',xdimid))
-CALL nc_check(nf90_inq_dimid(ncid,'south_north',ydimid))
-CALL nc_check(nf90_inquire_dimension(ncid,xdimid,dimname,idm_src))
-CALL nc_check(nf90_inquire_dimension(ncid,ydimid,dimname,jdm_src))
+  allocate(lon_src(idm_src, jdm_src))
+  allocate(lat_src(idm_src, jdm_src))
+  allocate(mask_src(idm_src, jdm_src))
 
-WRITE(*,*)'wrf2umwmgrid: WRF grid idm/jdm:',idm_src,jdm_src
+  call nc_check(nf90_inq_varid(ncid, 'XLONG', lonid))
+  call nc_check(nf90_inq_varid(ncid, 'XLAT', latid))
+  call nc_check(nf90_inq_varid(ncid, 'XLAND', maskid))
+  call nc_check(nf90_get_var(ncid, lonid, lon_src))
+  call nc_check(nf90_get_var(ncid, latid, lat_src))
+  call nc_check(nf90_get_var(ncid, maskid, mask_src))
+  call nc_check(nf90_close(ncid))
 
-ALLOCATE(lon_src(idm_src,jdm_src))
-ALLOCATE(lat_src(idm_src,jdm_src))
-ALLOCATE(mask_src(idm_src,jdm_src))
+  where (lon_src < 0) lon_src = lon_src + 360
 
-CALL nc_check(nf90_inq_varid(ncid,'XLONG',lonid))
-CALL nc_check(nf90_inq_varid(ncid,'XLAT',latid))
-CALL nc_check(nf90_inq_varid(ncid,'XLAND',maskid))
-CALL nc_check(nf90_get_var(ncid,lonid,lon_src))
-CALL nc_check(nf90_get_var(ncid,latid,lat_src))
-CALL nc_check(nf90_get_var(ncid,maskid,mask_src))
-CALL nc_check(nf90_close(ncid))
+  write(*,*)'wrf2umwmgrid: WRF grid longitude min/max:', &
+            minval(lon_src),maxval(lon_src)
+  write(*,*)'wrf2umwmgrid: WRF grid latitude min/max:', &
+            minval(lat_src),maxval(lat_src)
+  write(*,*)'wrf2umwmgrid: WRF grid number of sea points:', &
+            count(mask_src == 0)
+  write(*,*)'wrf2umwmgrid: WRF grid number of land points:', &
+            count(mask_src == 1)
 
-WHERE(lon_src < 0)lon_src = lon_src+360
+  ! Adjust land/sea values:
+  mask_src = mask_src - 1
 
-WRITE(*,*)'wrf2umwmgrid: WRF grid longitude min/max:',&
-          MINVAL(lon_src),MAXVAL(lon_src)
-WRITE(*,*)'wrf2umwmgrid: WRF grid latitude min/max:',&
-          MINVAL(lat_src),MAXVAL(lat_src)
-WRITE(*,*)'wrf2umwmgrid: WRF grid number of sea points:',&
-          COUNT(mask_src == 0)
-WRITE(*,*)'wrf2umwmgrid: WRF grid number of land points:',&
-          COUNT(mask_src == 1)
+  if (refinementFactor > 1) then
+    idm_dst = (idm_src - 1) * refinementFactor + 1
+    jdm_dst = (jdm_src - 1) * refinementFactor + 1
+  else if(refinementFactor < 0) then
+    idm_dst = idm_src / abs(refinementFactor)
+    jdm_dst = jdm_src / abs(refinementFactor)
+  else
+    idm_dst = idm_src
+    jdm_dst = jdm_src
+  end if
 
-! Adjust land/sea values:
-mask_src = mask_src-1
+  write(*,*) 'wrf2umwmgrid: UMWM grid idm/jdm:', idm_dst, jdm_dst
 
-!=======================================================================
+  allocate(lon_dst(idm_dst, jdm_dst))
+  allocate(lat_dst(idm_dst, jdm_dst))
+  allocate(mask_dst(idm_dst, jdm_dst))
 
-! 3) Refine or coarsen fields if requested:
+  lon_dst  = 0
+  lat_dst  = 0
+  mask_dst = 0
 
-IF(refinementFactor > 1)THEN
+  if (refinementFactor > 1) then ! refine:
+    call refineField(lon_src, lon_dst, refinementFactor)
+    call refineField(lat_src, lat_dst, refinementFactor)
+    call refineField(real(mask_src), mask_dst, refinementFactor)
+  else if(refinementFactor == 1) then ! copy:
+    lon_dst  = lon_src
+    lat_dst  = lat_src
+    mask_dst = mask_src
+  else ! coarsen:
+    call coarsenField(lon_src, lon_dst, - refinementFactor)
+    call coarsenField(lat_src, lat_dst, - refinementFactor)
+    call coarsenField(real(mask_src), mask_dst, - refinementFactor)
+  end if
 
-  idm_dst = (idm_src-1)*refinementFactor+1
-  jdm_dst = (jdm_src-1)*refinementFactor+1
+  write(*,*) 'wrf2umwmgrid: UMWM grid longitude min/max:', &
+             minval(lon_dst),maxval(lon_dst)
+  write(*,*) 'wrf2umwmgrid: UMWM grid latitude min/max:', &
+             minval(lat_dst),maxval(lat_dst)
+  write(*,*) 'wrf2umwmgrid: UMWM grid number of sea points:', &
+             count(nint(mask_dst) == 1)
+  write(*,*) 'wrf2umwmgrid: UMWM grid number of land points:', &
+             count(nint(mask_dst) == 0)
 
-ELSEIF(refinementFactor < 0)THEN
+  where (lon_dst > 180) lon_dst = lon_dst - 360
 
-  idm_dst = (idm_src-1)/FLOAT(ABS(refinementFactor))+1
-  jdm_dst = (jdm_src-1)/FLOAT(ABS(refinementFactor))+1
+  write(*,*) 'wrf2umwmgrid: Writing umwm.grid file.'
 
-ELSE
-  
-  idm_dst = idm_src
-  jdm_dst = jdm_src
+  call nc_check(nf90_create('umwm.grid', NF90_CLOBBER, ncid))
+  call nc_check(nf90_def_dim(ncid, 'x', idm_dst, xdimid))
+  call nc_check(nf90_def_dim(ncid, 'y', jdm_dst, ydimid))
+  call nc_check(nf90_def_var(ncid, 'lon', NF90_FLOAT, [xdimid,ydimid], lonid))
+  call nc_check(nf90_def_var(ncid, 'lat', NF90_FLOAT, [xdimid,ydimid], latid))
+  call nc_check(nf90_def_var(ncid, 'seamask', NF90_INT, [xdimid,ydimid], maskid))
+  call nc_check(nf90_put_att(ncid, NF90_GLOBAL, name='history', values='Generated by wrf2umwmgrid'))
+  call nc_check(nf90_enddef(ncid))
+  call nc_check(nf90_put_var(ncid, lonid, lon_dst))
+  call nc_check(nf90_put_var(ncid, latid, lat_dst))
+  call nc_check(nf90_put_var(ncid, maskid, nint(mask_dst)))
+  call nc_check(nf90_close(ncid))
 
-ENDIF
+  deallocate(lon_src, lat_src, mask_src)
+  deallocate(lon_dst, lat_dst, mask_dst)
 
-WRITE(*,*)'wrf2umwmgrid: UMWM grid idm/jdm:',idm_dst,jdm_dst
+  write(*,*) 'wrf2umwmgrid: Success.'
 
-ALLOCATE(lon_dst(idm_dst,jdm_dst))
-ALLOCATE(lat_dst(idm_dst,jdm_dst))
-ALLOCATE(mask_dst(idm_dst,jdm_dst))
-
-lon_dst  = 0
-lat_dst  = 0
-mask_dst = 0
-
-IF(refinementFactor > 1)THEN ! refine:
-
-  CALL refineField(lon_src,lon_dst,refinementFactor)
-  CALL refineField(lat_src,lat_dst,refinementFactor)
-  CALL refineField(FLOAT(mask_src),mask_dst,refinementFactor)
-
-ELSEIF(refinementFactor == 1)THEN ! copy:
-
-  lon_dst  = lon_src
-  lat_dst  = lat_src
-  mask_dst = mask_src
-
-ELSE ! coarsen:
-
-  CALL coarsenField(lon_src,lon_dst,-refinementFactor)
-  CALL coarsenField(lat_src,lat_dst,-refinementFactor)
-  CALL coarsenField(FLOAT(mask_src),mask_dst,-refinementFactor)
-
-ENDIF
-
-WRITE(*,*)'wrf2umwmgrid: UMWM grid longitude min/max:',&
-          MINVAL(lon_dst),MAXVAL(lon_dst)
-WRITE(*,*)'wrf2umwmgrid: UMWM grid latitude min/max:',&
-          MINVAL(lat_dst),MAXVAL(lat_dst)
-WRITE(*,*)'wrf2umwmgrid: UMWM grid number of sea points:',&
-          COUNT(NINT(mask_dst) == 1)
-WRITE(*,*)'wrf2umwmgrid: UMWM grid number of land points:',&
-          COUNT(NINT(mask_dst) == 0)
-
-WHERE(lon_dst > 180)lon_dst = lon_dst-360
-
-!=======================================================================
-
-! 4) Generate new umwm.grid file:
-WRITE(*,*)'wrf2umwmgrid: Writing umwm.grid file.'
-
-CALL nc_check(nf90_create('umwm.grid',nf90_clobber,ncid))
-CALL nc_check(nf90_def_dim(ncid,'x',idm_dst,xdimid))
-CALL nc_check(nf90_def_dim(ncid,'y',jdm_dst,ydimid))
-CALL nc_check(nf90_def_var(ncid,'lon',nf90_float,[xdimid,ydimid],lonid))
-CALL nc_check(nf90_def_var(ncid,'lat',nf90_float,[xdimid,ydimid],latid))
-CALL nc_check(nf90_def_var(ncid,'seamask',nf90_int,[xdimid,ydimid],maskid))
-CALL nc_check(nf90_put_att(ncid,NF90_GLOBAL,name='history',values='Generated by wrf2umwmgrid'))
-CALL nc_check(nf90_enddef(ncid))
-CALL nc_check(nf90_put_var(ncid,lonid,lon_dst))
-CALL nc_check(nf90_put_var(ncid,latid,lat_dst))
-CALL nc_check(nf90_put_var(ncid,maskid,NINT(mask_dst)))
-CALL nc_check(nf90_close(ncid))
-
-!=======================================================================
-
-! 5) Clean up:
-DEALLOCATE(lon_src,lat_src,mask_src)
-DEALLOCATE(lon_dst,lat_dst,mask_dst)
-
-WRITE(*,*)'wrf2umwmgrid: Success.'
-
-!=======================================================================
-ENDPROGRAM wrf2umwmgrid
+end program wrf2umwmgrid
