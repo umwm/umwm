706379df866e5eba9e56094cab436672c9770e09
milancurcic <caomaco@gmail.com>
2021-03-05 13:52:14 -0500

    add toml-f as a dependency

--- app/main.f90
+++ app/main.f90
@@ -1,12 +1,30 @@
 program umwm
   
+  use tomlf, only: toml_error, toml_parse, toml_table
   use umwm_domain, only: domain_type
   use umwm_grid, only: grid_type
   use umwm_spectrum, only: spectrum_type
   
   implicit none
 
+  integer :: unit
+  integer :: gridsize_x, gridsize_y
+  type(toml_error), allocatable :: error
+  type(toml_table), allocatable :: table
   type(domain_type) :: domain
+
+  open(newunit=unit, file='umwm.toml', status='old')
+  call toml_parse(table, unit, error)
+
+  if (allocated(error)) then
+    print *, error % stat, error % message
+    stop
+  end if
+
+  if (allocated(table)) then
+  end if
+
+  close(unit)
 
   domain = domain_type(grid_type(), spectrum_type())
 
