38aa561d370b5111e18f8dd295f939df4ca5d3eb
milancurcic <caomaco@gmail.com>
2021-03-08 14:59:10 -0500

    reading a few toml parameters

--- app/main.f90
+++ app/main.f90
@@ -1,33 +1,39 @@
 program umwm
   
-  use tomlf, only: toml_error, toml_parse, toml_table
+  use tomlf, only: toml_parse, toml_table, toml_array, get_value, toml_key
   use umwm_domain, only: domain_type
   use umwm_grid, only: grid_type
   use umwm_spectrum, only: spectrum_type
   
   implicit none
 
-  integer :: unit
-  integer :: gridsize_x, gridsize_y
-  type(toml_error), allocatable :: error
+  integer :: n, unit
+  integer :: grid_size_x, grid_size_y
   type(toml_table), allocatable :: table
+  type(toml_table), pointer :: domain_table
+  type(toml_key), allocatable :: keys(:)
   type(domain_type) :: domain
+  character(:), allocatable :: name
 
   open(newunit=unit, file='umwm.toml', status='old')
-  call toml_parse(table, unit, error)
-
-  if (allocated(error)) then
-    print *, error % stat, error % message
-    stop
-  end if
-
-  if (allocated(table)) then
-  end if
-
+  call toml_parse(table, unit)
   close(unit)
 
-  domain = domain_type(grid_type(), spectrum_type())
+  print *, 'Keys in umwm.toml:'
+  call table % get_keys(keys)
+  do n = 1, size(keys)
+    print *, n, keys(n) % key
+  end do
 
-  print *, 'Hello from umwm.'
+  call get_value(table, 'name', name)
+  call get_value(table, 'domain', domain_table)
+  call get_value(domain_table, 'grid_size_x', grid_size_x)
+  call get_value(domain_table, 'grid_size_y', grid_size_y)
+
+  print *, name
+  print *, grid_size_x
+  print *, grid_size_y
+  
+  domain = domain_type(grid_type(), spectrum_type())
 
 end program umwm
