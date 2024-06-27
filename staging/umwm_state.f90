5049a5ac8baa798b1837456a7f5a15d2276e06ad
milancurcic <caomaco@gmail.com>
2024-06-26 21:17:46 -0400

    Rename variance -> action

--- staging/umwm_state.f90
+++ staging/umwm_state.f90
@@ -9,7 +9,7 @@ module umwm_state
   public :: state_type
 
   type :: state_type
-    real, allocatable :: variance(:,:,:,:)
+    real, allocatable :: action(:,:,:,:)
   end type state_type
 
   interface state_type
@@ -24,12 +24,12 @@ contains
     type(spectrum_type), intent(in) :: spectrum
     integer :: stat
 
-    allocate(res % variance(spectrum % num_frequencies, &
-                            spectrum % num_directions, &
-                            grid % size_x, &
-                            grid % size_y), stat=stat)
-    if (stat /= 0) error stop 'Error allocating domain % variance.'
-    res % variance = 0
+    allocate(res % action(spectrum % num_frequencies, &
+                          spectrum % num_directions, &
+                          grid % size_x, &
+                          grid % size_y), stat=stat)
+    if (stat /= 0) error stop 'Error allocating domain % action.'
+    res % action = 0
 
   end function state_type_cons
 
